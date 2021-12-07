b506d82ff3c505cd35bdf8cbafcb4ca51d8d0c33
Mikhail Nepomnyashchiy <nem@kodix.ru>
2021-12-07 09:01:10 +0300

    hero added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import {Header} from 'components/Header';
+import {Hero} from 'components/Hero';
 import classes from './App.module.scss';
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
