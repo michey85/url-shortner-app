55911931bee2bb7e66b9da608db0f0c54bdda8c1
Mikhail Nepomnyashchiy <nem@kodix.ru>
2021-12-12 10:31:25 +0300

    from errors to view

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,23 +1,53 @@
+import {useForm} from 'react-hook-form'
 import {Button} from 'components/Button';
+
+import {URL_SHORTNER} from 'config';
 
 import classes from './Form.module.scss';
 
 const Form = () => {
+  const {
+    register,
+    formState: {errors},
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = ({Url}) => {
+    alert(Url)
+    fetch(URL_SHORTNER + Url, {
+      method: 'POST',
+    }).then(res => res.json()).then(console.log).catch(console.error)
+  }
+
   return (
     <section className={classes.section}>
       <div className="container">
-        <div className={classes.form}>
+        <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
           <input
             type="url"
             placeholder="Shorten a link here..."
             className={classes.input}
+            style={{
+              outlineColor: errors.Url ? 'var(--secondary-300)' : 'currentColor',
+              outlineWidth: errors.Url ? '4px' : '1px',
+            }}
+            {...register('Url', {
+              required: 'Please add a link',
+              pattern: {
+                value: /^(http|https):\/\/[^ "]+$/,
+                message: 'Please enter a valid url'
+              },
+            })}
           />
           <Button
             variant="square"
             type="submit"
             size="medium"
           >Shorten it!</Button>
-        </div>
+          {errors.Url && (
+            <div className={classes.error}>{errors.Url.message}</div>
+          )}
+        </form>
       </div>
     </section>
   )
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const URL_SHORTNER = 'https://api.shrtco.de/v2/shorten?url=';
+
+export const URL_INFO_BY_CODE = 'https://api.shrtco.de/v2/info?code=';
